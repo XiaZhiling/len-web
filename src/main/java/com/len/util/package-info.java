503e30472411bf4d4343bdcbff910a70b34f1434
zoran <xzl@xinlianpayment.com>
2016-06-13 12:06:03 +0800

    创建资源逻辑路径及包

---
+++ src/main/java/com/len/util/package-info.java
@@ -0,0 +1,14 @@
+/*
+ * Licensed to the personal for xzl
+ * https://github.com/XiaZhiling
+ * See the License for the specific language governing permissions and limitations
+ * under the License.
+ * @version v1.0
+ */
+/** 
+ * @ClassName: package-info 
+ * @Description: TODO(这里用一句话描述这个类的作用) 
+ * @author xzl<l576477316@foxmail.com>
+ * @date 2016年6月13日 上午11:59:04  
+ */
+package com.len.util;
