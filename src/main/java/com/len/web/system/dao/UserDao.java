2d6a336f59c3d65f602b6eb6d8fb6b0527766f3b
zoran <xzl@xinlianpayment.com>
2016-06-13 17:37:32 +0800

    pom 编写   配置修改

---
+++ src/main/java/com/len/web/system/dao/UserDao.java
@@ -0,0 +1,21 @@
+/*
+ * Licensed to the personal for xzl
+ * https://github.com/XiaZhiling
+ * See the License for the specific language governing permissions and limitations
+ * under the License.
+ * @version v1.0
+ */
+package com.len.web.system.dao;
+
+import com.len.core.generic.GenericDao;
+import com.len.web.system.model.User;
+
+/** 
+ * @ClassName: UserDao 
+ * @Description: 用户Dao接口 
+ * @author xzl<l576477316@foxmail.com>
+ * @date 2016年6月13日 下午4:54:19  
+ */
+public interface UserDao extends GenericDao<User, String> {
+	
+}
